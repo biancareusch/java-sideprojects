977a95bfaabd9192a08087a84906a8ba19446bf2
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-07 15:35:16 -0500

    finished TicTacToe game against CPU

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -19,24 +19,33 @@ public class TicTacToe {
             Scanner scan = new Scanner(System.in);
             System.out.println("Enter your placement (1-9): ");
             int playerPos = scan.nextInt();
-            while(playerPosition.contains(playerPos) || cpuPosition.contains(playerPos)) {
+            while (playerPosition.contains(playerPos) || cpuPosition.contains(playerPos)) {
                 System.out.println("position taken! Try again");
                 playerPos = scan.nextInt();
             }
 
             placePiece(gameBoard, playerPos, "player");
 
+            String result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+
             Random rand = new Random();
             int cpuPos = rand.nextInt(9) + 1;
-            while(playerPosition.contains(cpuPos) || cpuPosition.contains(cpuPos)) {
-                System.out.println("position taken! Try again");
+            while (playerPosition.contains(cpuPos) || cpuPosition.contains(cpuPos)) {
                 cpuPos = rand.nextInt(9) + 1;
             }
             placePiece(gameBoard, cpuPos, "cpu");
 
             printGameBoard(gameBoard);
-            String result = checkWinner();
-            System.out.println(result);
+
+            result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
         }
     }
 
@@ -93,25 +102,24 @@ public class TicTacToe {
             default:
                 break;
         }
-        printGameBoard(gameBoard);
+
     }
 
 
-
     public static String checkWinner() {
-        List topRow = Arrays.asList(1,2,3);
-        List midRow = Arrays.asList(4,5,6);
-        List botRow = Arrays.asList(7,8,9);
+        List topRow = Arrays.asList(1, 2, 3);
+        List midRow = Arrays.asList(4, 5, 6);
+        List botRow = Arrays.asList(7, 8, 9);
 
-        List leftCol = Arrays.asList(1,4,7);
-        List midCol = Arrays.asList(2,5,8);
-        List rightCol = Arrays.asList(3,6,9);
+        List leftCol = Arrays.asList(1, 4, 7);
+        List midCol = Arrays.asList(2, 5, 8);
+        List rightCol = Arrays.asList(3, 6, 9);
 
-        List cross1 = Arrays.asList(1,5,9);
-        List cross2 = Arrays.asList(3,5,7);
+        List cross1 = Arrays.asList(1, 5, 9);
+        List cross2 = Arrays.asList(3, 5, 7);
 
 
-        List<List>winning = new ArrayList<List>();
+        List<List> winning = new ArrayList<List>();
         winning.add(topRow);
         winning.add(midRow);
         winning.add(botRow);
@@ -121,12 +129,12 @@ public class TicTacToe {
         winning.add(cross1);
         winning.add(cross2);
 
-        for(List l : winning){
-            if(playerPosition.containsAll(l)){
+        for (List l : winning) {
+            if (playerPosition.containsAll(l)) {
                 return "Congratulations, you own!";
-            }else if(playerPosition.containsAll(l)){
+            } else if (playerPosition.containsAll(l)) {
                 return "CPU wins!";
-            }else if(playerPosition.size() + cpuPosition.size() == 9){
+            } else if (playerPosition.size() + cpuPosition.size() == 9) {
                 return "TIE!";
             }
         }
