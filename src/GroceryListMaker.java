611c6f9328b62da46040a34cf7300febb7ec796e
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-08 11:02:04 -0500

    added groceryItem class

---
+++ src/GroceryListMaker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GroceryListMaker {
+//    - before prompting the user to enter each item, ask how many items will be entered
+//    - for each item, the user should be prompted to enter in the name, quantity,
+//    and category of each grocery item
+//    - Create a `GroceryItem` class from which to instantiate `GroceryItem`
+//    objects to store in an array
+//    - once the user has entered all items, print out all items grouped by
+//    category and alphabetized in each group
+
+private String name;
+private int quantity;
+private String category;
+
+    public groceryItem(String name, int quantity, String category) {
+        this.name = name;
+        this.quantity = quantity;
+        this.category = category;
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many Items will you be entering? ");
+        int amountOfGroceries = scan.nextInt();
+
+
+    }
+
+
+
+
+}
