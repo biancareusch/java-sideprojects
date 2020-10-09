bd74641e86b6fe0dc8b9a9edddcdc5a699a7f794
Bianca Reusch <reusch.bianca@gmail.com>
2020-10-09 14:53:49 -0500

    added exercise instructions

--- src/musical/MusicTest.java
+++ src/musical/MusicTest.java
@@ -6,27 +6,53 @@ public class MusicTest {
 
     public static void main(String[] args) {
 
-Tuneable firstPiano = new Piano("strings",60);
-VolumeAdjustable firstSax = new Sax("brass","baritone");
+        Tuneable firstPiano = new Piano("strings", 60);
+        VolumeAdjustable firstSax = new Sax("brass", "baritone");
 
 
-firstPiano.tuneInstrument();
+        firstPiano.tuneInstrument();
 
 
-
-
-
-//        public static void detuneRetune(Tuneable tuneableObj) {
-//            tuneableObj.detuneInstrument();
-//            tuneableObj.tuneInstrument();
-//        }
+//      Create an abstract class called ```MusicalInstrument``` that implements a ```Playable``` interface.
 //
-//        public static void makeTwoStaccatoSounds(VolumeAdjustable va) {
-//            va.volumeUp();
-//            va.volumeDown();
-//            va.volumeUp();
-//            va.volumeDown();
-//        }
+//```MusicalInstrument``` should have a constructor that sets the value of a private string field of ```instrumentClassification``` (include getter and setter)
+//
+//The ```Playable``` interface contains the following method definitions:
+//
+//    public void startMusic();
+//    public void stopMusic();
+//
+//Create a ```Tuneable``` interface with the following method definitions:
+//
+//    public void tuneInstrument();
+//    public void detuneInstrument();
+//
+//Create a ```VolumeAdjustable``` interface with the following method definitions:
+//
+//    public void volumeUp();
+//    public void volumeDown();
+//
+//Create ```Piano``` and ```Sax``` classes that both extend from ```MusicInstrument``` and implement ```Tuneable``` and ```VolumeAdjustable```.
+//
+//For both ```Piano``` and ```Sax``` classes, implement all needed methods by sout'ing out messages that make sense.
+//
+//The ```Piano``` class should also include a private int field of ```numberOfKeys``` (update your constructor and add a getter and setter)
+//
+//The ```Sax``` class should also include a private int string field of ```saxType``` (update your constructor and add a getter and setter)
+//
+//Create a ```MusicTest``` class with a main method to practice instantiating ```Piano``` and ```Sax``` objects of type ```Piano``` and ```Sax``` or the types of the Interfaces they implement. Add the following two methods to the ```MusicTest``` class to experiement with various instances of the ```Piano``` and ```Sax``` objects:
+//
+//  	public static void detuneRetune(Tuneable tuneableObj) {
+//        tuneableObj.detuneInstrument();
+//        tuneableObj.tuneInstrument();
+//    }
+//
+//    public static void makeTwoStaccatoSounds(VolumeAdjustable va) {
+//        va.volumeUp();
+//        va.volumeDown();
+//        va.volumeUp();
+//        va.volumeDown();
+//    }
 
     }
 }
